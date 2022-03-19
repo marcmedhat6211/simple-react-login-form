b0b2da3ea2bc920ba2623c575bdf20b14b1490f8
Marc Medhat <marcmedhat6211@gmail.com>
2022-03-19 12:05:32 +0200

    explaining the imperativeHandle React hook

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -2,6 +2,14 @@ import React, { useRef, useImperativeHandle } from "react";
 
 import classes from "./Input.module.css";
 
+/**
+ * 99.9% of the times you won't need a scecond argument to the component, you will only need props
+ * but since we're accessing the input ref from outside the component, we need to pass it as an argument here
+ *
+ * forwardRef method:
+ *  this is a method coming from react which you have to use if you are using the ref of a component from outside the component like in this case
+ *  it takes the component as an argument
+ */
 const Input = React.forwardRef((props, ref) => {
   const inputRef = useRef();
 
@@ -9,6 +17,10 @@ const Input = React.forwardRef((props, ref) => {
     inputRef.current.focus();
   };
 
+  /**
+   * focus: is the externally available name which i'll use anywhere else
+   * activate: is a pointer to the function activate which by its turn accesses the input ref and focuses the input
+   */
   useImperativeHandle(ref, () => {
     return {
       focus: activate,
